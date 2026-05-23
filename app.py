d64ead6894fc3d8b082d6d193887e940ce11fff6
Your Name <you@example.com>
2026-05-23 02:12:36 +0200

    Update login and backend logic

--- app.py
+++ app.py
@@ -2139,4 +2139,4 @@ import uvicorn
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # ✅ Render impose son propre PORT
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.runapp.run(host='0.0.0.0', port=port, debug=False)
